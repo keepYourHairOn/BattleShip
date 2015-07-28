ca9aa49e21d3cabb5f828ab2d7888a913cbef550
Sofiia Yermolaieva <sofia-ne@ukr.net>
2015-07-29 00:34:02 +0300

    Added methods for outputting.
    Methods for outputting game players fields added.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Player player1 = new Human("Sofiia");
+        Player player2 = new Computer("Eva");
+
+        Sea sea = new Sea(player1, player2);
+    }
+}
